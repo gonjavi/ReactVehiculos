934e7a4d5b825207d2cea7e52996935c9886fc46
manci <javiercaliescali@gmail.com>
2021-03-24 10:21:46 -0500

    actualizacion funcionando

--- src/actions/actualizar.js
+++ src/actions/actualizar.js
@@ -4,18 +4,18 @@ export const CARRO_ACTUALIZAR_REQUEST = 'CARRO_ACTUALIZAR_REQUEST';
 export const CARRO_ACTUALIZAR_SUCCESS = 'CARRO_ACTUALIZAR_SUCCESS';
 export const CARRO_ACTUALIZAR_FAIL = 'CARRO_ACTUALIZAR_FAIL';
 
-const actualizar = (id, linea, marca, modelo, color, Foto) => async dispatch => {
+const actualizar = (id, linea, marca, modelo, color, foto) => async dispatch => {
   dispatch({
     type: CARRO_ACTUALIZAR_REQUEST,
     payload: {
-      linea, marca, modelo, color, Foto,
+      linea, marca, modelo, color, foto,
     },
   });
 
   try {
     const { data } = await axios.put(`http://localhost:3001/vehiculo/${id}`,
       {
-        linea, marca, modelo, color, Foto,
+        linea, marca, modelo, color, foto,
       });
     dispatch({ type: CARRO_ACTUALIZAR_SUCCESS, payload: data });
   } catch (error) {
